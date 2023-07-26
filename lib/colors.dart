61e7c93244c44eaea3919c2a213c288804e88702
nothing10101 <95474538+cyrDhanush@users.noreply.github.com>
2023-07-26 23:53:14 +0530

    logic done

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color bgwhite = Color(0xffF5F5F5);
+Color fgwhite = Color(0xffFFFFFF);
---
+++ lib/processor/unitconverter.dart
@@ -0,0 +1,23 @@
+class TempUnit {
+  double celcius = 0;
+  double fahrenheit = 0;
+  double kelvin = 0;
+
+  setCelcius(double cel) {
+    celcius = cel;
+    fahrenheit = (celcius * 9 / 5) + 32;
+    kelvin = celcius + 273.15;
+  }
+
+  setFahrenheit(double fah) {
+    fahrenheit = fah;
+    celcius = (fahrenheit - 32) * 5 / 9;
+    kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
+  }
+
+  setKelvin(double kel) {
+    kelvin = kel;
+    fahrenheit = (kelvin - 273.15) * 9 / 5 + 32;
+    celcius = kelvin - 273.15;
+  }
+}
